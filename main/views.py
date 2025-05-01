ae05f933ab6a482718a77abc0e12bb45f1da1c0a
green680trecker <cacanj14@gmail.com>
2025-05-01 13:54:48 +0300

    starting point

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello?")
+    return HttpResponse(f"[INFO!] <b>Status code: {HttpResponse.status_code}</b> <div>Good work</div>")
 
 def wish(request):
     return HttpResponse("<h1>I want to play the space station 13</h1>")
